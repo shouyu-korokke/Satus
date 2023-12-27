4878aa24a01b1b041515d391b9f262f26c57326a
shouyu-korokke <hongnachuan@gmail.com>
2023-12-27 21:08:38 +0100

    Add class for Character

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public string name;
+    public GameObject characterPrefab;
+    //public GameObject 2Dsprite;
+    public int attack_range;
+    public int attack_damage;
+    public int max_health;
+    public int movement_range;
+
+
+    //hidden
+    public int luck;
+    public bool extra_action;
+
+
+    //WIP
+    //public int armor;
+    //public 
+
+}
